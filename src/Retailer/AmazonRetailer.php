49a1a04a5e1eeb7f72deb5a4c7a77298bbb35c9b
Sascha Hemleb <sascha@hemleb.de>
2021-05-24 15:44:59 +0200

    Reduce accept header value

--- src/Retailer/AmazonRetailer.php
+++ src/Retailer/AmazonRetailer.php
@@ -29,7 +29,7 @@ class AmazonRetailer implements RetailerInterface
 				'headers' => [
 					'dnt' => '1',
 					'upgrade-insecure-requests' => '1',
-					'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0',
+					'accept' => 'text/html,application/xhtml+xml',
 					'referer' => 'https://www.amazon.com/',
 					'accept-language' => 'de-DE,de;q=0.9',
 					'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
